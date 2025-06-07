6e79f802fa80382b77fa4f02937c97c5c3555673
appydam <appydam@gmail.com>
2025-06-07 17:05:07 +0530

    new graphic for home page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -190,7 +190,7 @@ const Index = () => {
 
         {/* Image container for overlay */}
         <div className="relative -my-36 container mx-auto pb-10"> {/* Added container and adjusted margin/padding */}
-          <img src="/tt.png" alt="Background Image" className="w-full h-auto" />
+          <img src="/newhpg.png" alt="Background Image" className="w-full h-auto" />
         </div>
 
         {/* How SageCombat Works Section */}
