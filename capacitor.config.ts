805f0fccba51a88b24da6bf7f64ce245518c6e29
appydam <appydam@gmail.com>
2025-06-24 01:16:49 +0530

    integrating capacitor and did android, ios builds

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sagecombat.app',
+  appName: 'SageCombat',
+  webDir: 'dist'
+};
+
+export default config;
